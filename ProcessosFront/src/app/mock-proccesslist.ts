b8b59f655d37d6f09a730efdec832c2562e6e1c3
artn-dev <artn@cin.ufpe.br>
2022-04-04 17:55:01 -0300

    Displaying document list with mock documents

--- ProcessosFront/src/app/mock-proccesslist.ts
+++ ProcessosFront/src/app/mock-proccesslist.ts
@@ -4,14 +4,40 @@ export interface MockProccess {
   startDate: Date;
   judge: string;
   status: string;
+  documents: string[];
 }
 
 export const PROCCESSES: MockProccess[] = [
-  { id: 6, name: "Processo Judicial 7", startDate: new Date(2015, 8, 9), judge: "Fulano Detal", status: "concluído"},
-  { id: 5, name: "Processo Judicial 6", startDate: new Date(2012, 9, 17), judge: "Humberto Filho", status: "atrasado" },
-  { id: 4, name: "Processo Judicial 5", startDate: new Date(2009, 2, 16), judge: "Lucca di Francesco", status: "atrasado" },
-  { id: 3, name: "Processo Judicial 4", startDate: new Date(2008, 8, 11), judge: "Fulano Detal", status: "em andamento" },
-  { id: 2, name: "Processo Judicial 3", startDate: new Date(2007, 7, 12), judge: "Fulano Detal", status: "em andamento" },
-  { id: 1, name: "Processo Judicial 2", startDate: new Date(2006, 3, 10), judge: "Marco Sena", status: "concluído" },
-  { id: 0, name: "Processo Judicial 1", startDate: new Date(2005, 5, 18), judge: "Sergio Barreto", status: "atrasado" },
+  {
+    id: 6, name: "Processo Judicial 7", startDate: new Date(2015, 8, 9),
+    judge: "Fulano Detal", status: "concluído",
+    documents: []
+  },
+  { id: 5, name: "Processo Judicial 6", startDate: new Date(2012, 9, 17),
+    judge: "Humberto Filho", status: "atrasado",
+    documents: []
+  },
+  { id: 4, name: "Processo Judicial 5", startDate: new Date(2009, 2, 16),
+    judge: "Lucca di Francesco", status: "atrasado",
+    documents: []
+  },
+  { id: 3, name: "Processo Judicial 4", startDate: new Date(2008, 8, 11),
+    judge: "Fulano Detal", status: "em andamento",
+    documents: []
+  },
+  { id: 2, name: "Processo Judicial 3", startDate: new Date(2007, 7, 12),
+    judge: "Fulano Detal", status: "em andamento",
+    documents: []
+  },
+  { id: 1, name: "Processo Judicial 2", startDate: new Date(2006, 3, 10),
+    judge: "Marco Sena", status: "concluído",
+    documents: []
+  },
+  { id: 0, name: "Processo Judicial 1", startDate: new Date(2005, 5, 18),
+    judge: "Sergio Barreto", status: "atrasado",
+    documents: [
+      "Documento 1", "Documento 2", "Documento 3", "Documento 4", "Documento 5",
+      "Documento 6", "Documento 7", "Documento 8", "Documento 9", "Documento 10",
+    ]
+  },
 ]
