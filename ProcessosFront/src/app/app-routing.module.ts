da4add53f8e0a1c8f82b541799d7535ecaff7e21
artn-dev <artn@cin.ufpe.br>
2022-04-04 12:40:04 -0300

    Setup routing for Profile component

--- ProcessosFront/src/app/app-routing.module.ts
+++ ProcessosFront/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginFormComponent } from './login-form/login-form.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
   { path: "login", component: LoginFormComponent },
+  { path: "perfil", component: ProfileComponent },
 ];
 
 @NgModule({
