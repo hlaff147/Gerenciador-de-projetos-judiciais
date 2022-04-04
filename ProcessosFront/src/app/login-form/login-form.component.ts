bde0fd7d91ed296c5c1d8d253ca80e116fb17b4b
artn-dev <artn@cin.ufpe.br>
2022-04-04 01:46:11 -0300

    Basic login data validation

--- ProcessosFront/src/app/login-form/login-form.component.ts
+++ ProcessosFront/src/app/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -7,13 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  cpf = "";
-  password = "";
+  cpf: string = "";
+  password: string = "";
   rememberUser = false;
+  cpfInvalid = false;
+  submitted = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  cpfIsValid(): boolean {
+      return !(this.cpfInvalid || this.cpf === "");
+  }
+
+  passwordIsValid(): boolean {
+    return this.password !== "";
+  }
+
+  loginUser(): void {
+    this.submitted = true;
+    if (!this.cpfIsValid())
+      return ;
+
+    if (!this.passwordIsValid())
+      return ;
+
+    this.router.navigate(['/']);
+  }
+
+  reset(): void {
+    this.cpf = "";
+    this.password = "";
+    this.rememberUser = false;
+    this.submitted = false
+  }
+
+  updateCpf(event: any): void {
+    this.cpfInvalid = isNaN(event);
+  }
 }
