58a7069b22b91962cad2fb2bdbe6ddb936c4e3ea
artn-dev <artn@cin.ufpe.br>
2022-04-04 11:34:23 -0300

    Setup routing for SignupForm component

--- ProcessosFront/src/app/app-routing.module.ts
+++ ProcessosFront/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 
 const routes: Routes = [
   { path: "login", component: LoginFormComponent },
+  { path: "cadastro", component: SignupFormComponent },
 ];
 
 @NgModule({
