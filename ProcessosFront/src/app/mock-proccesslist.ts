e4992dea05811aac0c39fa8a8cf739b56cb0e135
artn-dev <artn@cin.ufpe.br>
2022-04-04 13:04:44 -0300

    Add new property status to MockProccess interface

--- ProcessosFront/src/app/mock-proccesslist.ts
+++ ProcessosFront/src/app/mock-proccesslist.ts
@@ -2,14 +2,15 @@ export interface MockProccess {
   name: string;
   startDate: Date;
   judge: string;
+  status: string;
 }
 
 export const PROCCESSES: MockProccess[] = [
-  { name: "Processo Judicial 7", startDate: new Date(2015, 8, 9), judge: "Fulano Detal" },
-  { name: "Processo Judicial 6", startDate: new Date(2012, 9, 17), judge: "Humberto Filho" },
-  { name: "Processo Judicial 5", startDate: new Date(2009, 2, 16), judge: "Lucca di Francesco" },
-  { name: "Processo Judicial 4", startDate: new Date(2008, 8, 11), judge: "Fulano Detal" },
-  { name: "Processo Judicial 3", startDate: new Date(2007, 7, 12), judge: "Fulano Detal" },
-  { name: "Processo Judicial 2", startDate: new Date(2006, 3, 10), judge: "Marco Sena" },
-  { name: "Processo Judicial 1", startDate: new Date(2005, 5, 18), judge: "Sergio Barreto" },
+  { name: "Processo Judicial 7", startDate: new Date(2015, 8, 9), judge: "Fulano Detal", status: "concluído"},
+  { name: "Processo Judicial 6", startDate: new Date(2012, 9, 17), judge: "Humberto Filho", status: "atrasado" },
+  { name: "Processo Judicial 5", startDate: new Date(2009, 2, 16), judge: "Lucca di Francesco", status: "atrasado" },
+  { name: "Processo Judicial 4", startDate: new Date(2008, 8, 11), judge: "Fulano Detal", status: "em andamento" },
+  { name: "Processo Judicial 3", startDate: new Date(2007, 7, 12), judge: "Fulano Detal", status: "em andamento" },
+  { name: "Processo Judicial 2", startDate: new Date(2006, 3, 10), judge: "Marco Sena", status: "concluído" },
+  { name: "Processo Judicial 1", startDate: new Date(2005, 5, 18), judge: "Sergio Barreto", status: "atrasado" },
 ]
