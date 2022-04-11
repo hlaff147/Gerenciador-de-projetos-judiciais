70c80d40c2e3f4ce451cff6f8eef843351510278
artn-dev <artn@cin.ufpe.br>
2022-04-10 22:53:10 -0300

    [GPJ-20] Testa números em nome

--- ProcessosFront/cypress/integration/signup.spec.js
+++ ProcessosFront/cypress/integration/signup.spec.js
@@ -5,10 +5,15 @@ describe("Testes de signup", () => {
     cy.visit("cadastro");
   });
 
-  it("Clicar em item `cadastro` na barra de navegação abre página de cadastro", () => {
+  it("Clicar em item `Cadastro` na barra de navegação abre página de cadastro", () => {
     cy.visit("");
     cy.get("[data-cy='signup-link']").click();
     cy.url().should("include", "cadastro");
     cy.get("app-signup-form").should("be.visible");
+  });
+
+  it("Nome não deve conter números", () => {
+    cy.get("input[name=username]").type("Humbert0");
+    cy.get("[data-error='name-numbers']").should("be.visible");
   });
 });
