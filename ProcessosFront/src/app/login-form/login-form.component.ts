09fb3f4b02bc992001d28291c8bc893f57fe22cd
artn-dev <artn@cin.ufpe.br>
2022-04-13 21:23:55 -0300

    [GPJ-26] Login válido leva à lista de processos

--- ProcessosFront/src/app/login-form/login-form.component.ts
+++ ProcessosFront/src/app/login-form/login-form.component.ts
@@ -4,24 +4,22 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
-
-  cpf: string = "";
-  password: string = "";
+  cpf: string = '';
+  password: string = '';
   rememberUser = false;
   cpfInvalid = false;
   cpfLenght = false;
   submitted = false;
-  nameInvalid = false
-  constructor(private router: Router) { }
+  nameInvalid = false;
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   cpfIsValid(): boolean {
-      return !(this.cpfInvalid || this.cpf === "");
+    return !(this.cpfInvalid || this.cpf === '');
   }
 
   cpfLenghtIsValid(): boolean {
@@ -29,36 +27,32 @@ export class LoginFormComponent implements OnInit {
   }
 
   passwordIsValid(): boolean {
-    return this.password !== "";
+    return this.password !== '';
   }
 
   loginUser(): void {
     this.submitted = true;
-    if (!this.cpfIsValid())
-      return ;
+    if (!this.cpfIsValid()) return;
 
-    if (!this.passwordIsValid())
-      return ;
-          
-    if (!this.cpfLenghtIsValid())
-      return ;
+    if (!this.passwordIsValid()) return;
 
-    this.cpfLenght = this.cpfLenghtIsValid()
-    this.router.navigate(['/']);
+    if (!this.cpfLenghtIsValid()) return;
+
+    this.cpfLenght = this.cpfLenghtIsValid();
+    this.router.navigate(['/perfil']);
   }
 
   reset(): void {
-    this.cpf = "";
-    this.password = "";
+    this.cpf = '';
+    this.password = '';
     this.rememberUser = false;
-    this.submitted = false
+    this.submitted = false;
   }
 
   updateCpf(event: any): void {
     this.cpfInvalid = isNaN(event);
   }
   updateName(event: any): void {
-    this.cpfInvalid = event.search(/\d+/) === -1
+    this.cpfInvalid = event.search(/\d+/) === -1;
   }
-  
 }
