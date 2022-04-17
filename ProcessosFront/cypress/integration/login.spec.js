60767ef71d2e18e63933b9b7e8dc1af3056821c7
artn-dev <artn@cin.ufpe.br>
2022-04-17 15:13:57 -0300

    [GPJ-28] Atualiza testes de login

--- ProcessosFront/cypress/integration/login.spec.js
+++ ProcessosFront/cypress/integration/login.spec.js
@@ -5,20 +5,30 @@ describe("Testes de login", () => {
     cy.visit("login");
   });
 
-  it("Login não pode ser efetuado com campos vazios", () => {
-    cy.get("[data-btn='login']").first().click();
-    cy.get("[data-error='cpf-missing']").should("be.visible");
-    cy.get("[data-error='password-missing']").should("be.visible");
+  it("Submissão de campos vazios gera mensagens de erro", () => {
+    cy.get("[data-btn='login']").click();
+    cy.get("[data-error='cpf-missing']").should("be.visible").and("be.visible");
+    cy.get("[data-error='password-missing']")
+      .should("be.visible")
+      .and("be.visible");
   });
 
   it("CPF não pode conter letras", () => {
     cy.get("input[name=cpf]").type("1111111111a");
-    cy.get("[data-error='cpf-letters']").should("be.visible");
+    cy.get("input[name=cpf]").should("have.value", "111.111.111-1");
   });
 
   it("CPF deve ter 11 dígitos", () => {
     cy.get("input[name=cpf]").type("1111111111");
-    cy.get("[data-btn='login']").first().click();
+    cy.get("[data-btn='login']").click();
     cy.get("[data-error='cpf-length']").should("be.visible");
+  });
+
+  it("Login com dados válidos não gera mensagens de erro", () => {
+    cy.get("input[name=cpf]").type("11111111111");
+    cy.get("input[name=password]").type("Senha1234");
+    cy.get("[data-btn='login']").click();
+    cy.get("[data-error='cpf-missing']").should("not.exist");
+    cy.get("[data-error='password-missing']").should("not.exist");
   });
 });
