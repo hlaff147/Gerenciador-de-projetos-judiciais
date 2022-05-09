402a7038d8c71874123848996b64636ac176bf91
artn-dev <artn@cin.ufpe.br>
2022-05-09 04:38:36 -0300

    [GPJ-30] Adiciona pontos de api para processos

--- ProcessosBack/server.ts
+++ ProcessosBack/server.ts
@@ -8,6 +8,15 @@ import {
 } from "./knex/querries/users";
 import { db } from "./knex/config/database";
 import { User } from "../common/user";
+import {
+  createProcess,
+  deleteProcess,
+  getAllProcess as getAllProcesses,
+  getProcessById,
+  getProcessesByJudgeId,
+  getProcessesByLaywerId,
+} from "./knex/querries/processes";
+import { Process } from "../common/process";
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -49,7 +58,7 @@ app.get("/api/usuarios", async (req: Request, res: Response) => {
   }
 });
 
-app.delete("/api/apagar", async (req: Request, res: Response) => {
+app.delete("/api/apagar-usuario", async (req: Request, res: Response) => {
   const cpf: string = req.body.cpf;
   const success = await deleteUser(cpf);
 
@@ -72,6 +81,83 @@ app.get("/api/auth", async (req: Request, res: Response) => {
     res.send({ success: user });
   } else {
     res.send({ failure: "Não pode encontrar usuário com essas credenciais" });
+  }
+});
+
+app.post("/api/abrir-processo", async (req: Request, res: Response) => {
+  var process = req.body;
+  const processId = await createProcess(process);
+
+  if (processId) {
+    process = await getProcessById(processId);
+    console.log(
+      `[SERVIDOR] Processo ${process.name} foi registrado com id ${processId}`
+    );
+    res.send({ success: process });
+  } else {
+    res.send({ failure: "Processo não pode ser aberto" });
+  }
+});
+
+app.delete("/api/apagar-processo", async (req: Request, res: Response) => {
+  const id: number = req.body.id;
+  const success = await deleteProcess(id);
+
+  if (success) {
+    console.log(`[SERVIDOR] Processo id ${id} foi deletado da base de dados`);
+    res.send({ success: `Processo id ${id} deletado com sucesso` });
+  } else {
+    res.send({ failure: `Não pode deletar processo id: ${id}` });
+  }
+});
+
+app.get("/api/processos", async (req: Request, res: Response) => {
+  const lawyerId = req.query.lawyerId as string;
+  const judgeId = req.query.judgeId as string;
+
+  let processes: Process[] | null = null;
+
+  console.log(lawyerId, judgeId);
+
+  if (!lawyerId && !judgeId) {
+    processes = await getAllProcesses();
+    console.log(`[SERVIDOR] Buscando ${processes?.length} processos`);
+  } else if (lawyerId !== "") {
+    processes = await getProcessesByLaywerId(parseInt(lawyerId));
+    console.log(
+      `[SERVIDOR] Buscando ${processes?.length} processos de advogado id ${lawyerId}`
+    );
+  } else if (judgeId !== "") {
+    processes = await getProcessesByJudgeId(parseInt(judgeId));
+    console.log(
+      `[SERVIDOR] Buscando ${processes?.length} processos de juiz id ${judgeId}`
+    );
+  }
+
+  if (processes) {
+    res.send({ success: processes });
+  } else {
+    res.send({ failure: "Não pode buscar processos" });
+  }
+});
+
+app.get("/api/processo", async (req: Request, res: Response) => {
+  const id = req.query.id as string;
+
+  if (!id) {
+    res.send({
+      failure: "Por favor, ofereça uma forma de identificar o processo",
+    });
+    return;
+  }
+
+  const process = await getProcessById(parseInt(id));
+
+  if (process) {
+    console.log(`[SERVIDOR] Buscando processos ${id}`);
+    res.send({ success: process });
+  } else {
+    res.send({ failure: `Não pode encontrar processos com id ${id}` });
   }
 });
 
