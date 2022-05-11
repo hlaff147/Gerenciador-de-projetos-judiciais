e445f69b5a2e7b55307777b90889c16e0a9b2a5b
artn-dev <artn@cin.ufpe.br>
2022-05-11 09:52:07 -0300

    [GPJ-42] Modal de confirmação ao apagar conta

--- ProcessosFront/src/components/profile/profile.component.ts
+++ ProcessosFront/src/components/profile/profile.component.ts
@@ -1,8 +1,9 @@
-import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { UserService } from 'src/services/user.service';
 import { User } from '../../../../common/user';
+import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-profile',
@@ -12,7 +13,11 @@ import { User } from '../../../../common/user';
 export class ProfileComponent implements OnInit {
   user: User | undefined;
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.user = this.userService.getCurrUser();
@@ -34,8 +39,20 @@ export class ProfileComponent implements OnInit {
   }
 
   deleteAccount(): void {
-    this.userService
-      .deleteUser(this.user)
-      .subscribe(() => this.router.navigate(['/cadastro']));
+    const dialogRef = this.dialog.open(ConfirmModalComponent, {
+      width: '30rem',
+      data: {
+        message:
+          'Tem certeza que quer apagar a sua conta? Você poderá criar outra conta depois, mas perderá acesso a quaisquer processos que tenha autorado.',
+        confirm: 'Sim, tenho certeza',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result)
+        this.userService
+          .deleteUser(this.user)
+          .subscribe(() => this.router.navigate(['/cadastro']));
+    });
   }
 }
