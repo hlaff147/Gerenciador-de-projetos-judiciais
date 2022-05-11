8f3911b20df02ccbef245d8f9e699c42040580a5
artn-dev <artn@cin.ufpe.br>
2022-05-10 21:26:32 -0300

    [GPJ-37] Adiciona configurações de testes

--- ProcessosBack/knexfile.ts
+++ ProcessosBack/knexfile.ts
@@ -1,10 +1,20 @@
 import type { Knex } from "knex";
+const path = require("path");
 
 const config: { [key: string]: Knex.Config } = {
+  test: {
+    client: "sqlite3",
+    connection: ":memory:",
+    migrations: {
+      directory: path.join(__dirname, "knex", "migrations"),
+    },
+    useNullAsDefault: true,
+    //debug: true,
+  },
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./dev.sqlite3",
+      filename: "dev.sqlite3",
     },
     useNullAsDefault: true,
     debug: true,
