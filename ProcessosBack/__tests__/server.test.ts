c5529989ea6bb33c5ae8d538bdce53f4b8ff8076
artn-dev <artn@cin.ufpe.br>
2022-05-10 21:27:29 -0300

    [GPJ-37] Adiciona teste de cadastro

---
+++ ProcessosBack/__tests__/server.test.ts
@@ -0,0 +1,28 @@
+import { server } from "../server";
+import { db } from "../knex/config/database";
+import { User } from "../../common/user";
+import * as request from "supertest";
+
+describe("Testes dos pontos de api relativos a usuários", () => {
+  beforeAll(async () => {
+    await db.migrate.latest();
+  });
+
+  it("Cadastro de usuário com informações válidas", async () => {
+    const user: User = {
+      name: "Saul Goodman",
+      cpf: "11111111111",
+      email: "bettercallsaul@fake.com",
+      phone: "11111111111",
+      role: "advogado",
+      password: "Senha1234",
+    };
+
+    const res = await request(server)
+      .post("/api/cadastrar")
+      .send(user)
+      .set("Accept", "application/json");
+
+    expect(res.body).toHaveProperty("success");
+  });
+});
