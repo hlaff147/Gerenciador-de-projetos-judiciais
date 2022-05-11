0520f6f92425296ad8068cf9f4e81456bd92bea0
artn-dev <artn@cin.ufpe.br>
2022-05-11 08:42:13 -0300

    [STAGING] Corrige chamada de UserService.getCurrUser()

--- ProcessosFront/src/components/profile/profile.component.ts
+++ ProcessosFront/src/components/profile/profile.component.ts
@@ -15,7 +15,7 @@ export class ProfileComponent implements OnInit {
   constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
-    this.userService.getCurrUser().subscribe((user) => (this.user = user));
+    this.user = this.userService.getCurrUser();
   }
 
   formattedCpf(): string {
